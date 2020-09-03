989f55a6b214d9001ec5de36c26b321c3b1f937d
Nalen98 <52778977+Nalen98@users.noreply.github.com>
2020-09-03 13:15:44 +0300

    Apply patched bytes from Ghidra's listing

--- src/main/java/angryghidra/AngryGhidraPopupMenu.java
+++ src/main/java/angryghidra/AngryGhidraPopupMenu.java
@@ -1,15 +1,25 @@
 package angryghidra;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import docking.action.MenuData;
+import docking.widgets.textfield.IntegerTextField;
 import ghidra.app.context.ListingActionContext;
 import ghidra.app.context.ListingContextAction;
 import ghidra.app.plugin.core.colorizer.ColorizingService;
 import ghidra.framework.plugintool.PluginTool;
 import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressIterator;
 import ghidra.program.model.listing.Program;
+import ghidra.program.model.mem.MemoryAccessException;
 
 public class AngryGhidraPopupMenu extends ListingContextAction {
     public final String MenuName = "AngryGhidraPlugin";
@@ -34,7 +44,7 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
 
         tool.setMenuGroup(new String[] {
             MenuName
-        }, Group_Name);
+        }, Group_Name);             
 
         CurrentAvoidAddrses = new ArrayList < Address > ();
         ListingContextAction SetFind = new ListingContextAction("Set Find Address", getName()) {
@@ -68,7 +78,7 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
                 Address address = context.getLocation().getAddress();
                 UnSetColor(address);
                 CurrentFindAddr = null;
-                AngryGhidraProvider.TFFind.setText("");
+               AngryGhidraProvider.TFFind.setText("");
 
             }
         };
@@ -92,7 +102,7 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
                 CurrentBlankAddr = address;
                 SetColor(address, Color.CYAN);
                 AngryGhidraProvider.chckbxBlankState.setSelected(true);
-                AngryGhidraProvider.TFBlankState.setText("0x" + address.toString());
+               AngryGhidraProvider.TFBlankState.setText("0x" + address.toString());
             }
         };
 
@@ -111,7 +121,7 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
                 Address address = context.getLocation().getAddress();
                 UnSetColor(address);
                 CurrentBlankAddr = null;
-                AngryGhidraProvider.TFBlankState.setText("");
+               AngryGhidraProvider.TFBlankState.setText("");
                 AngryGhidraProvider.chckbxBlankState.setSelected(false);
 
             }
@@ -180,7 +190,100 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
             "Unset",
             "Avoid Address"
         }, null, Group_Name));
-        tool.addAction(UnSetAvoid);
+        tool.addAction(UnSetAvoid); 
+        
+        ListingContextAction ApplyPatchedBytes = new ListingContextAction("Apply Patched Bytes", getName()) {
+
+            @Override
+            protected void actionPerformed(ListingActionContext context) {
+                    
+            	Address MinAddress = context.getSelection().getMinAddress();
+                AddressIterator addressRange = context.getSelection().getAddresses(true);
+                StringBuilder HexStringBuilder = new StringBuilder();
+                for (Address address: addressRange) {
+                	byte Byte = 0;
+					try {
+						Byte = context.getProgram().getMemory().getByte(address);
+					} catch (MemoryAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	HexStringBuilder.append(String.format("%02X", Byte));                	
+                }  
+                String HexValueString = HexStringBuilder.toString();                
+                BigInteger HexValue = new BigInteger(HexValueString, 16);
+                
+                if (AngryGhidraProvider.TFstore_addr.getText().isEmpty() == false) {
+                	
+                	IntegerTextField TFaddr = new IntegerTextField();
+                	TFaddr.setHexMode();
+                	TFaddr.setValue(MinAddress.getOffset());
+                    GridBagConstraints gbc_TFaddr = new GridBagConstraints();
+                    gbc_TFaddr.fill = GridBagConstraints.HORIZONTAL;
+                    gbc_TFaddr.anchor = GridBagConstraints.NORTH;
+                    gbc_TFaddr.gridx = 1;
+                    gbc_TFaddr.insets = new Insets(0, 0, 0, 5);
+                    gbc_TFaddr.gridy = AngryGhidraProvider.GuiStoreCounter;
+                    gbc_TFaddr.weightx = 1;
+                    gbc_TFaddr.weighty = 0.1;
+                    AngryGhidraProvider.WMPanel.add(TFaddr.getComponent(), gbc_TFaddr);                
+                    AngryGhidraProvider.TFStoreAddrs.add(TFaddr);
+
+                    IntegerTextField TFval = new IntegerTextField();
+                    TFval.setHexMode();
+                    TFval.setValue(HexValue);
+                    GridBagConstraints gbc_TFval = new GridBagConstraints();
+                    gbc_TFval.fill = GridBagConstraints.HORIZONTAL;
+                    gbc_TFval.anchor = GridBagConstraints.NORTH;
+                    gbc_TFval.insets = new Insets(0, 0, 0, 5);
+                    gbc_TFval.gridx = 3;
+                    gbc_TFval.gridy = AngryGhidraProvider.GuiStoreCounter;
+                    gbc_TFval.weightx = 1;
+                    gbc_TFval.weighty = 0.1;
+                    AngryGhidraProvider.WMPanel.add(TFval.getComponent(), gbc_TFval);                
+                    AngryGhidraProvider.TFStoreVals.add(TFval);
+
+                    JButton btnDel = new JButton("");
+                    btnDel.setBorder(null);
+                    btnDel.setContentAreaFilled(false);
+                    btnDel.setIcon(new ImageIcon(getClass().getResource("/images/edit-delete.png")));
+                    GridBagConstraints gbc_btnDel = new GridBagConstraints();
+                    gbc_btnDel.fill = GridBagConstraints.HORIZONTAL;
+                    gbc_btnDel.anchor = GridBagConstraints.NORTH;
+                    gbc_btnDel.insets = new Insets(0, 0, 0, 5);
+                    gbc_btnDel.gridx = 0;
+                    gbc_btnDel.gridy = AngryGhidraProvider.GuiStoreCounter++;
+                    gbc_btnDel.weighty = 0.1;				
+                    AngryGhidraProvider.WMPanel.add(btnDel, gbc_btnDel);
+                    AngryGhidraProvider.delStore.add(btnDel);
+                    btnDel.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                        	AngryGhidraProvider.GuiStoreCounter--;
+                        	AngryGhidraProvider.WMPanel.remove(TFaddr.getComponent());
+                        	AngryGhidraProvider.WMPanel.remove(TFval.getComponent());
+                        	AngryGhidraProvider.WMPanel.remove(btnDel);
+                        	AngryGhidraProvider.delStore.remove(btnDel);
+                        	AngryGhidraProvider.TFStoreAddrs.remove(TFaddr);
+                        	AngryGhidraProvider.TFStoreVals.remove(TFval);
+                        	AngryGhidraProvider.WMPanel.repaint();
+                        	AngryGhidraProvider.WMPanel.revalidate();
+                        }
+
+                    });
+                    AngryGhidraProvider.WMPanel.repaint();
+                    AngryGhidraProvider.WMPanel.revalidate();
+                }            
+                else {
+                	AngryGhidraProvider.TFstore_addr.setValue(MinAddress.getOffset());
+                	AngryGhidraProvider.TFstore_val.setValue(HexValue);                	
+                }
+                
+            }
+        };   
+        ApplyPatchedBytes.setPopupMenuData(new MenuData(new String[] {
+                MenuName,
+                "Apply Patched Bytes"}, null, Group_Name));
+        tool.addAction(ApplyPatchedBytes);
 
     }
 
@@ -201,4 +304,5 @@ public class AngryGhidraPopupMenu extends ListingContextAction {
         program.endTransaction(TransactionID, true);
 
     }
+    
 }
