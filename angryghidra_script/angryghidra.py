a575aa2c1795dc93e571a609e6b74e2d28f7017f
Nalen98 <52778977+Nalen98@users.noreply.github.com>
2020-09-03 13:13:10 +0300

    Writing to memory and hooks

--- angryghidra_script/angryghidra.py
+++ angryghidra_script/angryghidra.py
@@ -4,13 +4,31 @@ import claripy
 import json
 import time
 
-EXPLORE_OPT = {}  #Explore options
-REGISTERS = [] #Main registers of your binary
+EXPLORE_OPT = {}  # Explore options
+REGISTERS = []  # Main registers of your binary
+SYMVECTORS = []
+
+
+def hook_function(state):
+    for object in EXPLORE_OPT["Hooks"]:
+        for frame in object.items():
+            if frame[0] == str(hex(state.solver.eval(state.regs.ip))):
+                for option, data in frame[1].items():
+                    if "sv" in data:
+                        symbvector_length = int(data[2:], 0)
+                        symbvector = claripy.BVS('symvector', symbvector_length * 8)
+                        SYMVECTORS.append(symbvector)
+                        data = symbvector
+                    else:
+                        data = int(str(data), 0)
+                    for REG in REGISTERS:
+                        if REG == option:
+                            setattr(state.regs, option, data)
+                            break
 
 
 def main(file):
-
-    with open(file) as json_file:
+    with open(file, encoding='utf-8') as json_file:
         global EXPLORE_OPT
         EXPLORE_OPT = json.load(json_file)
 
@@ -47,7 +65,7 @@ def main(file):
     else:
         state = p.factory.entry_state()
 
-# Memory spaces to set
+    # Store symbolic vectors in memory
     if "Memory" in EXPLORE_OPT:
         Memory = {}
         for addr, length in EXPLORE_OPT["Memory"].items():
@@ -57,14 +75,40 @@ def main(file):
             symb_vector = claripy.BVS('input', symbmem_len * 8)
             state.memory.store(symbmem_addr, symb_vector)
 
-# Handle Symbolic Registers (if you know actually which registers are necessary to store your data)
+    # Write to memory
+    if "Store" in EXPLORE_OPT:
+        for addr, value in EXPLORE_OPT["Store"].items():
+            store_addr = int(addr, 16)
+            store_value = int(value, 16)
+            store_length = len(value) - 2
+            state.memory.store(store_addr, state.solver.BVV(store_value, 4 * store_length))
+
+    # Handle Symbolic Registers
     if "Registers" in EXPLORE_OPT:
         for register, data in EXPLORE_OPT["Registers"].items():
-            data = int(str(data), 0)
+            if "sv" in data:
+                symbvector_length = int(data[2:], 0)
+                symbvector = claripy.BVS('symvector', symbvector_length * 8)
+                SYMVECTORS.append(symbvector)
+                data = symbvector
+            else:
+                data = int(str(data), 0)
             for REG in REGISTERS:
                 if REG == register:
                     setattr(state.regs, register, data)
                     break
+
+    # Handle Hooks
+    if "Hooks" in EXPLORE_OPT:
+        for object in EXPLORE_OPT["Hooks"]:
+            for frame in object.items():
+                hook_address = frame[0]
+                for option, data in frame[1].items():
+                    data = int(str(data), 0)
+                    if option == "Length":
+                        hook_length = data
+                        break
+                p.hook(int(hook_address, 16), hook_function, length=hook_length)
 
     simgr = p.factory.simulation_manager(state)
     if "avoid" in locals():
@@ -86,23 +130,27 @@ def main(file):
         win_sequence = win_sequence[:-1]
         print("Trace:" + win_sequence)
 
-        #If argv was used
         if len(argv) > 1:
             for i in range(1, len(argv)):
                 print("argv[{id}] = {solution}".format(id=i, solution=found_path.solver.eval(argv[i], cast_to=bytes)))
 
-        # Print value from user's memory space set
         if "Memory" in locals() and len(Memory) != 0:
             for address, length in Memory.items():
-                print("{addr} = {value}".format(addr=hex(address), value=found_path.solver.eval(found_path.memory.load(address, length), cast_to=bytes)))
+                print("{addr} = {value}".format(addr=hex(address),
+                                                value=found_path.solver.eval(found_path.memory.load(address, length),
+                                                                             cast_to=bytes)))
 
-        # STDIN
+        if len(SYMVECTORS) > 0:
+            for SV in SYMVECTORS:
+                print(found_path.solver.eval(SV, cast_to=bytes))
+
         found_stdins = found_path.posix.stdin.content
         if len(found_stdins) > 0:
             std_id = 1
             for stdin in found_stdins:
                 print(
-                    "stdin[{id}] = {solution}".format(id=std_id, solution=found_path.solver.eval(stdin[0], cast_to=bytes)))
+                    "stdin[{id}] = {solution}".format(id=std_id,
+                                                      solution=found_path.solver.eval(stdin[0], cast_to=bytes)))
                 std_id += 1
     else:
         print("")
